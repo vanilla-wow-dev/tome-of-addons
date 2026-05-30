9955ddb85f5e3208767c4444af160e871b6d25f1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-05-30 19:18:47 +0200

    feat(client): detect wow version & build (#3)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod exe;
+mod wow;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -10,7 +7,10 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            wow::detect_wow_roots_command,
+            exe::inspect_wow_exe_command
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
